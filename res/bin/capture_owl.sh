14387ee94e78a37ec6e5258dd01c9ffb489c74cb
Andrew Black <isabelle@rfbevan.co.uk>
2024-09-16 16:26:43 +0100

    Improve capture settings

--- res/bin/capture_owl.sh
+++ res/bin/capture_owl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-ssh "$1" "libcamera-still --viewfinder-mode 9248:6944:10:P --viewfinder-buffer-count 1 --denoise cdn_off --autofocus-mode manual --lens-position " "$2" " --shutter 500000 -o /tmp/owlsight.jpg"
+ssh "$1" "libcamera-still --viewfinder-mode 9248:6944:10:P --viewfinder-buffer-count 1 --denoise cdn_off --autofocus-mode manual --lens-position " "$2" " --shutter 200000 -o /tmp/photo.jpg"
 rm -f /tmp/photo.jpg
 rsync --progress "$1":/tmp/photo.jpg /tmp/
 feh /tmp/photo.jpg
